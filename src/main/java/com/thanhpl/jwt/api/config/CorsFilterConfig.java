ca0003cc33804467d917022c3cdfa5c2ad239278
Phan Le Thanh <Thanh-Le.Phan@aia.com>
2018-10-15 12:45:47 +0700

    Add configuration

--- src/main/java/com/thanhpl/jwt/api/config/CorsFilterConfig.java
+++ src/main/java/com/thanhpl/jwt/api/config/CorsFilterConfig.java
@@ -2,10 +2,12 @@ package com.thanhpl.jwt.api.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 public class CorsFilterConfig {
 	/**
      * Allowing all origins, headers and methods here is only intended to keep this example simple.
